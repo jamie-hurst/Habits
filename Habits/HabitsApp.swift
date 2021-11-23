202ba78aafbf91fe6207b46b0282f2c335cf4027
warmrome <pxpx786@gmail.com>
2021-11-23 11:13:09 -0800

    Initial Commit

---
+++ Habits/HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by Jameson Hurst on 11/23/21.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
